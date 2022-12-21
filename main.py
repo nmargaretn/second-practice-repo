8750607b9753aea0a3b69fa2af4cd1cc631eba74
0hhelga <okyivska@kse.org.ua>
2022-12-21 22:30:41 +0200

    cod was fixed (update)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 
+file_save = True
 
 file_name = sys.argv[1]
 
@@ -94,8 +95,9 @@ if sys.argv[2] == "-overall":
     lines = lines[1:]
 
     if "-output" in sys.argv:
-        output_file = open(sys.argv[sys.argv.index("-output") + 1], 'w')
+        result_file = open(sys.argv[sys.argv.index("-output") + 1], 'w')
         countries = sys.argv[3:sys.argv.index("-output")]
+        file_save = False
     else:
         countries = sys.argv[3:]
 
@@ -125,11 +127,13 @@ if sys.argv[2] == "-overall":
 
         if "-output" in sys.argv:
             print(country, current_year, medals)
-            print(country, current_year, medals, file=output_file)
+            print(country, current_year, medals, file=result_file)
+
         else:
             print(country, current_year, medals)
 
-
+    if "-output" in sys.argv:
+        result_file.close()
 
 
 
@@ -141,73 +145,81 @@ if argument == "-medals":
 else:
     years = sys.argv[3]
 
-output = len(sys.argv) == 7
-if output:
-    output_name = sys.argv[6]
-    output_file = open(output_name, 'w')
+if file_save:
+    if "-output" in sys.argv:
+        output_name = sys.argv[sys.argv.index("-output") + 1]
+        output_file = open(output_name, 'w')
 
 
-line = file.readline()
-counter = 0
-c_gold = 0
-c_silver = 0
-c_bronze = 0
-dictionary = {}
-years_dictionary = {}
-while line:
-    line = line[:-1]
-    word = line.split('\t')
-    country = word[6]
-    country_code = word[7]
-    year = word[9]
-    if (country == countries or country_code == countries) and year == years:
-        if counter < 10 and word[14] != "NA":
-            counter += 1
-            if word[14] == "Gold":
-                c_gold += 1
-            elif word[14] == "Silver":
-                c_silver += 1
-            elif word[14] == "Bronze":
-                c_bronze += 1
-            print(f"{word[1]}, {word[12]}, {word[14]}")
-            if output:
-                print(f"{word[1]}, {word[12]}, {word[14]}", file=output_file)
-
-    if argument == "-total" and word[14] != "NA" and year == sys.argv[3]:
-        keys = country
-        values = ["Gold", "Silver", "Bronze"]
-        # if country in dict, get list and add
-        # else - add new list
-        if country not in dictionary:
-            dictionary[country] = [0, 0, 0]
-            if values[0] == word[14]:
-                dictionary[country][0] = 1
-            elif values[1] == word[14]:
-                dictionary[country][1] = 1
-            elif values[2] == word[14]:
-                dictionary[country][2] = 1
-        else:
-            if country in dictionary:
-                if values[0] == word[14]:
-                    dictionary[country][0] += 1
-                elif values[1] == word[14]:
-                    dictionary[country][1] += 1
-                elif values[2] == word[14]:
-                    dictionary[country][2] += 1
-                else:
-                    dictionary[country] = 1
-
-    curent_year = word[9]
     line = file.readline()
+    counter = 0
+    c_gold = 0
+    c_silver = 0
+    c_bronze = 0
+    dictionary = {}
+    years_dictionary = {}
+    while line:
+        line = line[:-1]
+        word = line.split('\t')
+        country = word[6]
+        country_code = word[7]
+        year = word[9]
+        if (country == countries or country_code == countries) and year == years:
+            if counter < 10 and word[14] != "NA":
+                counter += 1
+                if word[14] == "Gold":
+                    c_gold += 1
+                elif word[14] == "Silver":
+                    c_silver += 1
+                elif word[14] == "Bronze":
+                    c_bronze += 1
+                if "-output" in sys.argv:
+                    print(f"{word[1]}, {word[12]}, {word[14]}")
+                    print(f"{word[1]}, {word[12]}, {word[14]}", file=output_file)
+                else:
+                    print(f"{word[1]}, {word[12]}, {word[14]}")
 
-if argument == "-medals":
-    print(f"Gold:{c_gold}, Silver:{c_silver}, Bronze:{c_bronze}")
+        if argument == "-total" and word[14] != "NA" and year == sys.argv[3]:
+            keys = country
+            values = ["Gold", "Silver", "Bronze"]
+            # if country in dict, get list and add
+            # else - add new list
+            if country not in dictionary:
+                dictionary[country] = [0, 0, 0]
+                if values[0] == word[14]:
+                    dictionary[country][0] = 1
+                elif values[1] == word[14]:
+                    dictionary[country][1] = 1
+                elif values[2] == word[14]:
+                    dictionary[country][2] = 1
+            else:
+                if country in dictionary:
+                    if values[0] == word[14]:
+                        dictionary[country][0] += 1
+                    elif values[1] == word[14]:
+                        dictionary[country][1] += 1
+                    elif values[2] == word[14]:
+                        dictionary[country][2] += 1
+                    else:
+                        dictionary[country] = 1
+
+        curent_year = word[9]
+        line = file.readline()
+
+    if argument == "-medals":
+        print(f"Gold:{c_gold}, Silver:{c_silver}, Bronze:{c_bronze}")
 
 
-if argument == "-total":
-    for country in dictionary:
-        print(f"{country}\t\t Gold:{dictionary[country][0]}, Silver:{dictionary[country][1]}, Bronze:{dictionary[country][2]}")
+    if argument == "-total":
+        for country in dictionary:
+            if "-output" in sys.argv:
+                print(
+                    f"{country}\t\t Gold:{dictionary[country][0]}, Silver:{dictionary[country][1]}, Bronze:{dictionary[country][2]}")
 
-if output:
-    print(f"Gold:{c_gold}, Silver:{c_silver}, Bronze:{c_bronze}", file=output_file)
-    output_file.close()
+                print(f"{country}\t\t Gold:{dictionary[country][0]}, Silver:{dictionary[country][1]}, Bronze:{dictionary[country][2]}", file=output_file)
+            else:
+                print(f"{country}\t\t Gold:{dictionary[country][0]}, Silver:{dictionary[country][1]}, Bronze:{dictionary[country][2]}")
+
+    if "-output" in sys.argv:
+        print(f"Gold:{c_gold}, Silver:{c_silver}, Bronze:{c_bronze}", file=output_file)
+        output_file.close()
